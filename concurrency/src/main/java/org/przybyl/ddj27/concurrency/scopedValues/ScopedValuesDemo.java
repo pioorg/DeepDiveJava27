pioorg/DeepDiveJava26

Initial commit.

concurrency/src/main/java/org/przybyl/ddj27/concurrency/scopedValues/ScopedValuesDemo.java
@@ -1,0 +1,99 @@
+/*
+ *  Copyright (C) 2023 Piotr Przybył
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package org.przybyl.ddj27.concurrency.scopedValues;
+
+
+import java.util.Random;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.StructuredTaskScope;
+
+// Don't forget ot use --enable-preview
+public class ScopedValuesDemo {
+
+    static final ScopedValue<Integer> SECURITY_CLEARANCE_LEVEL = ScopedValue.newInstance();
+
+    public static void main(String[] args) {
+
+        int level = levelFromRequest();
+
+        var voyager = new Starship();
+
+        ScopedValue.where(SECURITY_CLEARANCE_LEVEL, level)
+            .run(() -> {
+                doSomeStuffOn(voyager);
+
+                System.out.println(" -- let's jail current character");
+                ScopedValue.where(SECURITY_CLEARANCE_LEVEL, levelForPrisoner())
+                    .run(() -> doSomeStuffOn(voyager));
+
+                System.out.println(" -- and let's try again");
+                doSomeStuffOn(voyager);
+
+            });
+
+    }
+
+    private static void doSomeStuffOn(Starship starship) {
+        System.out.println("Acting with security clearance level " + SECURITY_CLEARANCE_LEVEL.orElse(-1));
+        try (var scope = StructuredTaskScope.open()) {
+            scope.fork(starship::turnLightsOn);
+            scope.fork(() -> starship.locateCrewMember("Tuvok"));
+            scope.fork(starship::blockBridgeControls);
+            scope.join();
+        } catch (InterruptedException | ExecutionException ie) {
+            // please keep in mind this is demo only, don't do that at home!
+            throw new RuntimeException(ie);
+        }
+    }
+
+    private static int levelFromRequest() {
+        return new Random().nextInt(10);
+    }
+
+    private static int levelForPrisoner() {
+        return 0;
+    }
+
+    static class Starship {
+        Random deckGen = new Random();
+
+        boolean turnLightsOn() {
+            System.out.println("Lights on");
+            return true;
+        }
+
+        boolean blockBridgeControls() {
+            if (SECURITY_CLEARANCE_LEVEL.isBound() && SECURITY_CLEARANCE_LEVEL.get() >= 10) {
+                System.out.println("Bridge controls blocked");
+                return true;
+            } else {
+                System.out.println("Insufficient security level");
+                return false;
+            }
+        }
+
+        boolean locateCrewMember(String name) {
+            if (SECURITY_CLEARANCE_LEVEL.isBound() && SECURITY_CLEARANCE_LEVEL.get() >= 2) {
+                System.out.printf("Crew member %s is on deck %d%n", name, deckGen.nextInt(1, 17));
+                return true;
+            } else {
+                System.out.println("Insufficient security level");
+                return false;
+            }
+        }
+    }
+}

goodies/src/main/java/org/przybyl/ddj27/stableValues/CurrentWeatherTeller.java
@@ -1,0 +1,55 @@
+/*
+ *  Copyright (C) 2025 Piotr Przybył
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.przybyl.ddj27.stableValues;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.util.stream.Gatherers;
+import java.util.stream.Stream;
+
+/// don't forget `--enable-preview`
+public class CurrentWeatherTeller {
+
+    private final LazyConstant<HttpClient> httpClient = LazyConstant.of(() -> HttpClient.newBuilder().connectTimeout(Duration.ofSeconds(30)).build());
+
+    private HttpClient getHttpClient() {
+        return httpClient.get();
+    }
+
+    void main() {
+        Stream.of(
+                "https://api.open-meteo.com/v1/forecast?latitude=50.45&longitude=30.52&current_weather=true", //Kyiv
+                "https://api.open-meteo.com/v1/forecast?latitude=52.23&longitude=21.01&current_weather=true" //Warsaw
+            )
+            .gather(Gatherers.mapConcurrent(5, uri -> HttpRequest.newBuilder().uri(URI.create(uri)).build()))
+            .gather(Gatherers.mapConcurrent(5, this::sendHttpReq))
+            .map(HttpResponse::body)
+            .forEach(IO::println);
+    }
+
+    private HttpResponse<String> sendHttpReq(HttpRequest req) {
+        try {
+            return getHttpClient().send(req, HttpResponse.BodyHandlers.ofString());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}

pioorg/DeepDiveJava27

concurrency/src/main/java/org/przybyl/ddj27/concurrency/scopedValues/ScopedValuesDemo.java
@@ -1,0 +1,99 @@
+/*
+ *  Copyright (C) 2023 Piotr Przybył
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package org.przybyl.ddj27.concurrency.scopedValues;
+
+
+import java.util.Random;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.StructuredTaskScope;
+
+// Don't forget ot use --enable-preview
+public class ScopedValuesDemo {
+
+    static final ScopedValue<Integer> SECURITY_CLEARANCE_LEVEL = ScopedValue.newInstance();
+
+    public static void main(String[] args) {
+
+        int level = levelFromRequest();
+
+        var voyager = new Starship();
+
+        ScopedValue.where(SECURITY_CLEARANCE_LEVEL, level)
+            .run(() -> {
+                doSomeStuffOn(voyager);
+
+                System.out.println(" -- let's jail current character");
+                ScopedValue.where(SECURITY_CLEARANCE_LEVEL, levelForPrisoner())
+                    .run(() -> doSomeStuffOn(voyager));
+
+                System.out.println(" -- and let's try again");
+                doSomeStuffOn(voyager);
+
+            });
+
+    }
+
+    private static void doSomeStuffOn(Starship starship) {
+        System.out.println("Acting with security clearance level " + SECURITY_CLEARANCE_LEVEL.orElse(-1));
+        try (var scope = StructuredTaskScope.open()) {
+            scope.fork(starship::turnLightsOn);
+            scope.fork(() -> starship.locateCrewMember("Tuvok"));
+            scope.fork(starship::blockBridgeControls);
+            scope.join();
+        } catch (InterruptedException | ExecutionException e) {
+            // please keep in mind this is demo only, don't do that at home!
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static int levelFromRequest() {
+        return new Random().nextInt(10);
+    }
+
+    private static int levelForPrisoner() {
+        return 0;
+    }
+
+    static class Starship {
+        Random deckGen = new Random();
+
+        boolean turnLightsOn() {
+            System.out.println("Lights on");
+            return true;
+        }
+
+        boolean blockBridgeControls() {
+            if (SECURITY_CLEARANCE_LEVEL.isBound() && SECURITY_CLEARANCE_LEVEL.get() >= 10) {
+                System.out.println("Bridge controls blocked");
+                return true;
+            } else {
+                System.out.println("Insufficient security level");
+                return false;
+            }
+        }
+
+        boolean locateCrewMember(String name) {
+            if (SECURITY_CLEARANCE_LEVEL.isBound() && SECURITY_CLEARANCE_LEVEL.get() >= 2) {
+                System.out.printf("Crew member %s is on deck %d%n", name, deckGen.nextInt(1, 17));
+                return true;
+            } else {
+                System.out.println("Insufficient security level");
+                return false;
+            }
+        }
+    }
+}

goodies/src/main/java/org/przybyl/ddj27/lazyConstants/CurrentWeatherTeller.java
@@ -1,0 +1,57 @@
+/*
+ *  Copyright (C) 2025 Piotr Przybył
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.przybyl.ddj27.lazyConstants;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.util.stream.Gatherers;
+import java.util.stream.Stream;
+
+/// don't forget `--enable-preview`
+public class CurrentWeatherTeller {
+
+    private final LazyConstant<HttpClient> httpClient =
+        LazyConstant.of(() ->
+            HttpClient.newBuilder().connectTimeout(Duration.ofSeconds(30)).build());
+
+    private HttpClient getHttpClient() {
+        return httpClient.get();
+    }
+
+    void main() {
+        Stream.of(
+                "https://api.open-meteo.com/v1/forecast?latitude=50.45&longitude=30.52&current_weather=true", //Kyiv
+                "https://api.open-meteo.com/v1/forecast?latitude=52.23&longitude=21.01&current_weather=true" //Warsaw
+            )
+            .gather(Gatherers.mapConcurrent(5, uri -> HttpRequest.newBuilder().uri(URI.create(uri)).build()))
+            .gather(Gatherers.mapConcurrent(5, this::sendHttpReq))
+            .map(HttpResponse::body)
+            .forEach(IO::println);
+    }
+
+    private HttpResponse<String> sendHttpReq(HttpRequest req) {
+        try {
+            return getHttpClient().send(req, HttpResponse.BodyHandlers.ofString());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}